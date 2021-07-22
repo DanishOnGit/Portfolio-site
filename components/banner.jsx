b1e062f0cd3964b951c6814bf9e52a64ea10ae74
DanishOnGit <youcandoit1911@gmail.com>
2021-07-22 22:32:23 +0530

    style:navlink colors

--- components/banner.jsx
+++ components/banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
       <p>Hi,</p>
       <h1 className="text-3xl md:text-6xl text font-semibold">I am <span className="text-red-700">Danish</span> Ahmed.</h1>
       <h1 className="text-2xl md:text-6xl text font-semibold text-gray-500">a fullstack web developer</h1>
-      <p className="hidden md:block mt-4 text-xl">I like to make things for the web. It all started out of curiosity. But now, it has become a passion. I love playing chess too!</p>
+      <p className="hidden md:block mt-4 text-xl">I like to make things for the web. It all started out of curiosity. But now, it has become a passion. I love playing <span className="font-semibold">Chess</span> too!</p>
     </div>
   );
 };
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,17 +4,17 @@ import Link from "next/link";
   return (
     <nav className="flex flex-col md:flex-row justify-center md:justify-between items-center max-w-4xl mx-auto h-20 ">
       <Link href="/" passHref>
-        <div className="mb-2 md:mb-0 text-2xl font-semibold">Danish A.</div>
+        <div className="mb-2 md:mb-0 text-2xl font-semibold cursor-pointer">Danish A.</div>
       </Link>
-      <ul className="flex list-none text-red-700 ">
+      <ul className="flex list-none">
         <Link href="/" passHref>
-          <li className="mx-2 md:mx-5 cursor-pointer hover:text-red-500">Home</li>
+          <li className="mx-2 md:mx-5 cursor-pointer hover:text-red-700">Home</li>
         </Link>
         <Link href="/projects" passHref>
-          <li className="mx-2 md:mx-5 cursor-pointer hover:text-red-500">Projects</li>
+          <li className="mx-2 md:mx-5 cursor-pointer hover:text-red-700">Projects</li>
         </Link>
         <Link href="/blogs" passHref>
-          <li className="mx-2 cursor-pointer hover:text-red-500">Blogs</li>
+          <li className="mx-2 cursor-pointer hover:text-red-700">Blogs</li>
         </Link>
       </ul>
     </nav>
