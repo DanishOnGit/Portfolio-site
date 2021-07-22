117d7cf5ebe7e0085faf81657101a80243b5f480
DanishOnGit <youcandoit1911@gmail.com>
2021-07-23 01:20:35 +0530

    style: blog card

--- components/blogCard.jsx
+++ components/blogCard.jsx
@@ -5,8 +5,8 @@ const BlogCard=({blogPost:{title,description,shortDescription,date,duration,link
  return (
     <div className="text-left p-2.5 w-full m-auto my-2  leading-6">
     <h2 className="text-xl md:text-2xl mb-2 font-medium">{title}</h2>
-    <small>{date}</small>
-    <small>{duration}</small>
+    <small>{date} </small>|
+    <small className="mx-1">{duration}</small>
     <br/>
     <p>{description}</p>
     <a href={link}><button className=" px-4 py-1 mt-4 rounded-none bg-black  text-white">Read</button></a>
