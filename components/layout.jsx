8771a733a972154e9b6a9522dfdf3b1cc572aa32
DanishOnGit <youcandoit1911@gmail.com>
2021-07-25 14:22:09 +0530

    add:more projects added

--- components/layout.jsx
+++ components/layout.jsx
@@ -5,7 +5,7 @@ import Navbar from "./navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="max-w-4xl mx-auto ">
+    <div className="max-w-4xl mx-auto">
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
